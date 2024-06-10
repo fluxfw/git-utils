ee7abef8a72884f16b52b941e220b961743f4664
fluxfw <fluxfw@bluewin.ch>
2024-06-10 15:46:47 +0200

    Improve `echo`

--- clean-branches.sh
+++ clean-branches.sh
@@ -4,23 +4,23 @@ set -e
 
 path="$1"
 if [ -z "$path" ]; then
-    echo "Please pass a path!" >&2
+    echo Please pass a path! >&2
     exit 1
 fi
 
-echo "Your branches"
+echo Your branches
 git -C "$path" branch -a
 echo
 
-echo "Fetch remote branches"
+echo Fetch remote branches
 git -C "$path" fetch --all
 echo
 
-echo "Prune remote branches"
+echo Prune remote branches
 git -C "$path" remote prune origin
 echo
 
-echo "Check local changes"
+echo Check local changes
 git -C "$path" add .
 if [ -n "$(git -C "$path" diff --cached)" ]; then
     local_changes=true
@@ -31,16 +31,16 @@ git -C "$path" reset
 echo
 
 if [ "$local_changes" = false ]; then
-    echo "Switch to main and pull"
+    echo Switch to main and pull
     git -C "$path" switch main
     git -C "$path" pull
 else
-    echo "You have local changes"
-    echo "Skip witch to main"
+    echo You have local changes
+    echo Skip witch to main
 fi
 echo
 
-echo "Delete local branches"
+echo Delete local branches
 for b in `git -C "$path" branch --format="%(refname:short)" --merged main`; do
     if [ "$b" = main ] || [ "$b" = `git -C "$path" branch --show-current` ]; then
         continue
@@ -49,5 +49,5 @@ for b in `git -C "$path" branch --format="%(refname:short)" --merged main`; do
 done
 echo
 
-echo "Your branches"
+echo Your branches
 git -C "$path" branch -a
